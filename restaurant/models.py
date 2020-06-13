daa900369d10809414fa70d445c344abff35538f
“adityasingh931” <aditya.singh@ezedox.com>
2020-06-13 17:42:59 +0530

    models

--- restaurant/models.py
+++ restaurant/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class RestaurantDeshboardData(models.Model):
+
+    name = models.CharField(max_length=100, null=True,  blank=True, default=None)
+    img = models.ImageField(upload_to='pics')
+    desc = models.TextField(null=True,  blank=True, default=None)
+    price = models.IntegerField(null=True,  blank=True, default=None)
+    offer = models.BooleanField(default=False)
